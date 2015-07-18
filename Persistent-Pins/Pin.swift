9654a67258c9f3e44f43e74099b87f8b6056be94
Julius Danek <juliusdanek@gmail.com>
2015-07-17 18:48:05 -0700

    Step One Complete
    Completed Step One of my “Persistent Pin” Tutorial

---
+++ Persistent-Pins/Pin.swift
@@ -0,0 +1,37 @@
+//
+//  Pin.swift
+//  Persistent-Pins
+//
+//  Created by Julius Danek on 7/17/15.
+//  Copyright (c) 2015 Julius Danek. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import MapKit
+
+class Pin: NSManagedObject, MKAnnotation {
+
+    @NSManaged var latitude: NSNumber
+    @NSManaged var longitude: NSNumber
+    
+    override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+    
+    init(annotationLatitude: Double, annotationLongitude: Double, context: NSManagedObjectContext) {
+        
+        let entity = NSEntityDescription.entityForName("Pin", inManagedObjectContext: context)!
+        
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+        
+        latitude = NSNumber(double: annotationLatitude)
+        
+        longitude = NSNumber(double: annotationLongitude)
+    }
+    
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude as Double, longitude: longitude as Double)
+    }
+
+}
